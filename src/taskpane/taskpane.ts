7d8e0cdbd6c7e7f923b2d7d691c7c4b82f694013
gru-lucy <ha.badguybro@gmail.com>
2025-04-27 17:36:39 +0300

    fix taskpane:

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -7,7 +7,7 @@
 
 // The initialize function must be run each time a new page is loaded
 Office.onReady(() => {
-  document.getElementById("sideload-msg").style.display = "none";
+  // document.getElementById("sideload-msg").style.display = "block";
   document.getElementById("app-body").style.display = "flex";
   document.getElementById("run").onclick = run;
 });
